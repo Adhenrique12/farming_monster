c4dd5f5f385285025e814fea8ac8bd5d0862a42a
RaphGL <raphfl.dev@gmail.com>
2022-09-29 15:54:06 -0100

    fixed zoom issue and added start menu

--- Scripts/StartMenu/StartMenu.gd
+++ Scripts/StartMenu/StartMenu.gd
@@ -1,11 +1,10 @@
 extends Control
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-    pass # Replace with function body.
+@export var new_game: PackedScene
+@export var options: PackedScene
 
 func _on_new_game_button_pressed():
-    get_tree().change_scene_to_file("res://Scenes/Level0.tscn")
+    get_tree().change_scene_to_packed(new_game)
 
 
 func _on_quit_button_pressed():
